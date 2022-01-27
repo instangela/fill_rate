af8db2ef56361f1274ea412d9d3c87020eb9e524
Eric Hagman <eric@instawork.com>
2022-01-27 15:32:13 -0500

    fix: use stage to determine s3 filename

--- macros/unload_model_to_s3.sql
+++ macros/unload_model_to_s3.sql
@@ -3,19 +3,23 @@
 {% set iam_role %}arn:aws:iam::183605072238:role/RedshiftMLTransformsRole{% endset %}
 
 -- line up dev + prod prefix from lambda function
-{% set stage %}{% if this.target == "staging" %}dev{% else %}prod{% endif %}{% endset %}
-{% set s3_path %}s3://instawork-ml/transforms/{{ this.schema }}/{{ this.table }}__{% endset %}
+{% set stage %}{% if target.name == "staging" %}dev{% else %}prod{% endif %}{% endset %}
+{% set s3_path %}s3://instawork-ml/transforms/{{ stage }}/{{ this.table }}__{% endset %}
 
 {% set sql %}
 {{ redshift.unload_table(this.schema, this.table, iam_role=iam_role, s3_path=s3_path, header=True, delimiter='|', parallel=False, max_file_size='5 mb', overwrite=True) }}
 {% endset %}
 
 -- only unload for staging and production
-{% if this.target == "staging" or this.target == "production" %}
-    {% do log("Unloading feature " ~ this.table ~ " to " ~ s3_path ~ "*", info=True) %}
-    {{ sql }}
+{% if target.type == "redshift" %}
+    {% if target.name == "staging" or target.name == "production" %}
+        {% do log("Unloading feature " ~ this.table ~ " to " ~ s3_path ~ "*", info=True) %}
+        {{ sql }}
+    {% else %}
+        {% do log("Skipping unloading of feature " ~ this.table ~ " for " ~ this.schema, info=True) %} 
+    {% endif %}
 {% else %}
-    {% do log("Skipping unloading of feature " ~ this.table ~ " for " ~ this.schema, info=True) %} 
+    {% do log("UNLOAD is only supported by Redshift. Skipping unloading of feature " ~ this.table ~ " for " ~ this.schema, info=True) %}  
 {% endif %}
 
 {% endmacro %}
