d62f6f62711e49ced957693bde1c0165a17f1468
Eric Hagman <eric@instawork.com>
2022-02-01 10:25:43 -0500

    fix: business output doesn't need region for key

--- models/urgent_defect/output_business.sql
+++ models/urgent_defect/output_business.sql
@@ -8,7 +8,7 @@
 -- key, date, meta_features, binary_features, int_features, float_features, timestamp_features, categorical_features
 
 SELECT
-    (pbff.ID_worker_id || '_' || pbff.ID_business_id || '_' || pbff.MC_business_region) AS key,
+    (pbff.ID_worker_id || '_' || pbff.ID_business_id) AS key,
     CURRENT_DATE AS date,
     {{ dbt_utils.star(from=ref('pro_business_future_features'), except=["ID_worker_id", "ID_business_id", "MC_business_region", "ds"]) }},
     {{ dbt_utils.star(from=ref('pro_business_history_features'), except=["ID_worker_id", "ID_business_id", "MC_business_region", "ds"]) }}
